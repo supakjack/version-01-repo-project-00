4ff879d2ff928528fe8908c1095d0bcf5a97fa85
phukeiei <34035927+phukeiei@users.noreply.github.com>
2020-09-25 16:02:31 +0700

    update

--- joget-api-service/controllers/authController.js
+++ joget-api-service/controllers/authController.js
@@ -1,9 +1,12 @@
 const { authLoginSchema } = require('../helpers/validation_schema')
+const { accountModel } = require("../models/accountModel");
+
 module.exports = {
     login: async (req, res, next) => {
         try {
           const result = await authLoginSchema.validateAsync(req.body);
-          const data = result
+          const doesExist = await accountModel.findOne(result);
+          const data = doesExist
           res.status(200).send({ data });
         } catch (error) {
           next(error);
--- joget-api-service/models/accountModel.js
+++ joget-api-service/models/accountModel.js
@@ -1,35 +1,54 @@
-const mongoose = require('mongoose')
-const schema = mongoose.Schema
+const mongoose = require("mongoose");
+const { encode } = require("js-base64");
+const schema = mongoose.Schema;
 
 const accountSchema = new schema({
   name: {
     type: String,
-    required: true
+    required: true,
   },
   role: {
     type: String,
-    required: true
+    required: true,
   },
   url: {
     type: String,
-    required: true
+    required: true,
   },
   username: {
     type: String,
-    required: true
+    required: true,
   },
   password: {
     type: String,
-    required: true
+    required: true,
   },
   createDate: {
     type: Date,
-    default: Date.now
-  }
-})
+    default: Date.now,
+  },
+});
 
-const accountModel = mongoose.model('account', accountSchema)
+accountSchema.pre("save", async function (next) {
+  try {
+    console.log(this.password);
+    this.password = await encode(this.password);
+    console.log(this.password);
+  } catch (error) {
+    next(error);
+  }
+});
+
+accountSchema.pre("findOneAndUpdate", async function (next) {
+  try {
+    this._update.password = await encode(this._update.password);
+  } catch (error) {
+    next(error);
+  }
+});
+
+const accountModel = mongoose.model("account", accountSchema);
 module.exports = {
   accountModel,
-  accountSchema
-}
+  accountSchema,
+};
