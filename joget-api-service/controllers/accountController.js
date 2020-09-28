e987e6bd5d4f33228fa59be005b40aebb3922f96
phukeiei <34035927+phukeiei@users.noreply.github.com>
2020-09-28 16:58:09 +0700

    add cache

--- joget-api-service/controllers/accountController.js
+++ joget-api-service/controllers/accountController.js
@@ -1,16 +1,23 @@
 const { accountSchema, idSchema } = require("../helpers/validation_schema");
 const { accountModel } = require("../models/accountModel");
 const ObjectId = require("mongoose").Types.ObjectId;
-
+const client = require("../helpers/init_redis");
 module.exports = {
   getAccount: async (req, res, next) => {
     try {
       const result = await idSchema.validateAsync(req.query);
-      const doesExist = await accountModel.findOne({
-        _id: new ObjectId(result.id),
+      client.GET(result.id, async (error, query) => {
+        if (query) {
+          return res.send(JSON.parse(query));
+        }
+        const doesExist = await accountModel.findOne({
+          _id: new ObjectId(result.id),
+        });
+        const data = doesExist;
+        console.log(data);
+        client.SETEX(result.id, 600, JSON.stringify({data}));
+        res.send({data});
       });
-      const data = doesExist;
-      res.send({ data });
     } catch (error) {
       next(error);
     }
@@ -18,11 +25,18 @@ module.exports = {
   deleteAccount: async (req, res, next) => {
     try {
       const result = await idSchema.validateAsync(req.query);
+      // client.GET(result.id, async (error, query) => {
+      //   if (query) {
+      //     res.send("ลบไปแล้ว");
+      //     return;
+      //   }
       const doesExist = await accountModel.findOneAndRemove({
         _id: new ObjectId(result.id),
       });
       const data = doesExist;
+      // client.SET([result.id,data.username], JSON.stringify(data));
       res.status(204).send({ data });
+      // });
     } catch (error) {
       next(error);
     }
@@ -30,10 +44,19 @@ module.exports = {
   addAccount: async (req, res, next) => {
     try {
       const result = await accountSchema.validateAsync(req.body);
-      const account = new accountModel(result);
-      const saveAccount = await account.save();
-      const data = saveAccount;
-      res.status(201).send({ data });
+      console.log(result);
+      client.GET(result.username, async (error, query) => {
+        if (query) {
+          res.send("Username ซ้ำนะจ๊ะ");
+          return;
+        }
+        console.log("ไม่บอก");
+        const account = new accountModel(result);
+        const saveAccount = await account.save();
+        const data = saveAccount;
+        client.SETEX(result.username, 600, JSON.stringify({data}));
+        res.status(201).send({ data });
+      });
     } catch (error) {
       next(error);
     }
@@ -42,22 +65,16 @@ module.exports = {
     try {
       const resultBody = await accountSchema.validateAsync(req.body);
       const resultQuery = await idSchema.validateAsync(req.query);
-      const updateAccount = await accountModel.findByIdAndUpdate(new ObjectId(resultQuery.id),resultBody, {new: true})
+      const updateAccount = await accountModel.findByIdAndUpdate(
+        new ObjectId(resultQuery.id),
+        resultBody,
+        { new: true }
+      );
       const data = updateAccount;
+      client.SETEX(resultQuery.id, 600, JSON.stringify({data}));
       res.status(200).send({ data });
     } catch (error) {
       next(error);
     }
   },
-  // put: async (req, res) => {
-  //   try {
-  //     const resultBody = await accountSchema.validateAsync(req.body);
-  //     const resultParams = await idSchema.validateAsync(req.params);
-  //     Service.findByIdAndUpdate(req.params._id, req.body, (err, data) => {
-  //     if (err) return res.status(400).send(err);
-  //     res.status(200).send("อัพเดทข้อมูลเรียบร้อย");
-  //   });
-  // } catch (error){
-  //   next(error);
-  // }
 };
--- joget-api-service/controllers/authController.js
+++ joget-api-service/controllers/authController.js
@@ -1,15 +1,22 @@
-const { authLoginSchema } = require('../helpers/validation_schema')
+const { authLoginSchema } = require("../helpers/validation_schema");
 const { accountModel } = require("../models/accountModel");
+const client = require("../helpers/init_redis");
 
 module.exports = {
-    login: async (req, res, next) => {
-        try {
-          const result = await authLoginSchema.validateAsync(req.body);
-          const doesExist = await accountModel.findOne(result);
-          const data = doesExist
-          res.status(200).send({ data });
-        } catch (error) {
-          next(error);
+  login: async (req, res, next) => {
+    try {
+      const result = await authLoginSchema.validateAsync(req.body);
+      client.GET(result.name, async (error, query) => {
+        if (query) {
+          return res.send(JSON.parse(query));
         }
+      const doesExist = await accountModel.findOne(result);
+      const data = doesExist;
+      client.SETEX(data.name, 600, JSON.stringify({data}));
+      res.status(200).send({ data });
+      });
+    } catch (error) {
+      next(error);
     }
+  },
 };
